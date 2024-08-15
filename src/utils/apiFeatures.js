531a16e427a76a99fbb5bd67cdf7cff701b10390
Eman Radwan <emanalaa11499@gmail.com>
2024-08-15 16:36:26 +0300

    first commit

--- src/utils/apiFeatures.js
+++ src/utils/apiFeatures.js
@@ -5,7 +5,7 @@ export default class ApiFeatures {
   }
 
   pagination() {
-    const page = +this.query.page || 1;
+    let page = +this.query.page || 1;
     if (page > 1) page = 1;
     const limit = 2;
     const skip = (page - 1) * limit;
