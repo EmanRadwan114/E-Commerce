b8198737ec5a93a9cc197602cefdcb11ef5408b1
Eman Radwan <emanalaa11499@gmail.com>
2024-08-16 21:05:17 +0300

    first commit

--- src/utils/error handling/rollbackDelete.js
+++ src/utils/error handling/rollbackDelete.js
@@ -5,8 +5,8 @@ export const deleteFromCloudinary = asyncHandler(async (req, res, next) => {
   if (req?.filePath) {
     await cloudinary.api.delete_resources_by_prefix(req.filePath);
     await cloudinary.api.delete_folder(req.filePath);
-    next();
   }
+  next();
 });
 
 export const deleteFromDB = asyncHandler(async (req, res, next) => {
