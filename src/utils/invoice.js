0d08a0b69092f30df52d20bd607faa2851c412fc
Eman Radwan <emanalaa11499@gmail.com>
2024-08-16 21:09:12 +0300

    first commit

--- src/utils/invoice.js
+++ src/utils/invoice.js
@@ -15,7 +15,7 @@ export default async function createInvoice(invoice, path) {
 
 function generateHeader(doc) {
   doc
-    .image("../../public/assets/logo.png", 50, 45, { width: 50 })
+    .image("logo.png", 50, 45, { width: 50 })
     .fillColor("#444444")
     .fontSize(20)
     .text("Shop Cart", 110, 57)
